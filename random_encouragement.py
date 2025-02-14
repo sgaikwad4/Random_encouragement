dcad899691a965ebd307b0506ff54da2c41070ef
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-14 10:41:22 -0800

    Made messages and generated a random number

--- random_encouragement.py
+++ random_encouragement.py
@@ -4,9 +4,13 @@
 import random
 # Define a function with no parameters that returns a string
 def rand_encrgmnt():
-    print("hello")
 # Create four different message variables
+    mess_1 = print("Never give up")
+    mess_2 = print("Sebastion believes in you")
+    mess_3 = print("Jovan believes in you")
+    mess_4 = print("Arsalan believes in you")
 # Generate a random number from 1 to 4
+    rand_num = random.randint(1, 4)
 # Use if-elif statements to return the corresponding message
 # Call the function and print the result
 rand_encrgmnt()
