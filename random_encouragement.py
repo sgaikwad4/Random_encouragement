8b9aed853f90a5421c6a2e2c2d6aa5eec22aa770
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-14 10:34:10 -0800

    Made a function
    Need to add messages, generate random number, and use if - elif.

--- random_encouragement.py
+++ random_encouragement.py
@@ -1,8 +1,12 @@
 # TASK: Write a function that returns a random encouragement message.
 
 # Import the random module
+import random
 # Define a function with no parameters that returns a string
+def rand_encrgmnt():
+    print("hello")
 # Create four different message variables
 # Generate a random number from 1 to 4
 # Use if-elif statements to return the corresponding message
 # Call the function and print the result
+rand_encrgmnt()
